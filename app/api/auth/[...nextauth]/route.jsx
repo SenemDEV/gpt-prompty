28016eb9eebc9e3c4161c5835f8bf541dfba0924
SenemDEV <121942044+SenemDEV@users.noreply.github.com>
2023-09-04 20:10:49 +0200

    complete the route which handles sign in and and auth

--- app/api/auth/[...nextauth]/route.jsx
+++ app/api/auth/[...nextauth]/route.jsx
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import { connectToDB } from "@utils/database";
+import User from "@models/user";
 
 console.log({
   clientId: process.env.GOOGLE_ID,
@@ -14,22 +15,43 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
+      });
 
-  async signIn({ profile }) {
-    //serverless route -> lambda function -> only when called is being connected to the DB
+      session.user.id = sessionUser._id.toString();
+      return session;
+    },
 
-    try {
-      await connectToDB();
+    async signIn({ profile }) {
+      //serverless route -> lambda function -> only when called is being connected to the DB
 
-      // chaeck if a user already exists
+      try {
+        await connectToDB();
 
-      // if not, create a new user
+        // chaeck if a user already exists
+        const userExists = await User.findOne({
+          email: profile.email,
+        });
 
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+        // if not, create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
   },
 });
+
+export { handler as GET, handler as POST };
