4ebce5f087e83c2771628ac10afc321a57d3235b
SenemDEV <121942044+SenemDEV@users.noreply.github.com>
2023-09-04 16:09:32 +0200

    add nextouth/route and setup database

--- app/api/auth/[...nextauth]/route.jsx
+++ app/api/auth/[...nextauth]/route.jsx
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import { connectToDB } from "@utils/database";
 
 console.log({
   clientId: process.env.GOOGLE_ID,
@@ -15,5 +16,20 @@ const handler = NextAuth({
   ],
   async session({ session }) {},
 
-  async signIn({ profile }) {},
+  async signIn({ profile }) {
+    //serverless route -> lambda function -> only when called is being connected to the DB
+
+    try {
+      await connectToDB();
+
+      // chaeck if a user already exists
+
+      // if not, create a new user
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
 });
