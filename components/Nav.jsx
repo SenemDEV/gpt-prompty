0290f5f063b3a4828651ef6453caa996a936098d
SenemDEV <121942044+SenemDEV@users.noreply.github.com>
2023-09-18 14:49:38 +0200

    add form to create prompt

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -136,28 +136,3 @@ const Nav = () => {
 };
 
 export default Nav;
-
-////////////////////////////////////////////////////
-
-// import Link from "next/link";
-// import Image from "next/image";
-// import { useState, useEffect } from "react";
-// import { signIn, signOut, useSession, getProviders } from "next-auth/react";
-
-// const Nav = () => {
-//   return (
-//     <nav className="flex-between w-full mb-16 pt-3">
-//       <Link href="/" className="flex gap-2 flex-center">
-//         <Image
-//           src="/assets/images/logo.svg"
-//           alt="gpt-prompty Logo"
-//           width={80}
-//           height={60}
-//           className="object-contain"
-//         />
-//       </Link>
-//     </nav>
-//   );
-// };
-
-// export default Nav;
